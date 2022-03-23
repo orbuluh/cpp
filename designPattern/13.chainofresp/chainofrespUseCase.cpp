3aba6e5984971e730620940c737faaae9e4c0c69
orbuluh <scuendless@gmail.com>
2022-03-23 23:22:36 +0800

    adding cor example

--- designPattern/13.chainofresp/chainofrespUseCase.cpp
+++ designPattern/13.chainofresp/chainofrespUseCase.cpp
@@ -2,7 +2,41 @@
 #include <iostream>
 
 namespace chainofresp {
+
+struct DummyPasswordCheck : public UserCheck {
+    DummyPasswordCheck() = default;
+    ~DummyPasswordCheck() override = default;
+    bool check(User& user) override {
+        if (user.getPassword() != "backdoorpass!")
+            return false;
+        std::cout << "Password checked!" << std::endl;
+        return UserCheck::check(user);
+    }
+};
+
+struct DummyIpCheck : public UserCheck {
+    DummyIpCheck() = default;
+    ~DummyIpCheck() override = default;
+    bool check(User& user) override {
+        if (user.getSourceIp() != "0.0.0.0")
+            return false;
+        std::cout << "IP checked!" << std::endl;
+        return UserCheck::check(user);
+    }
+};
+
+
 void demo() {
-    
+
+    DummyPasswordCheck check;
+    DummyIpCheck subCheck;
+    check.add(dynamic_cast<UserCheck*>(&subCheck));
+
+    User experimentalUser("0.0.0.0", "backdoorpass!");
+    User actualUser("255.254.253.252", "yo");
+    bool res = check.check(experimentalUser);
+    std::cout << res << std::endl;
+    res = check.check(actualUser);
+    std::cout << res << std::endl;
 }
 } // namespace chainofresp
--- designPattern/13.chainofresp/chainofrespUseCase.h
+++ designPattern/13.chainofresp/chainofrespUseCase.h
@@ -1,4 +1,26 @@
 #pragma once
+#include <iostream>
 namespace chainofresp {
+class User {
+public:
+    User(std::string ip, std::string pw) : sourceIp_(ip), password_(pw) {}
+    std::string getSourceIp() const { return sourceIp_; }
+    std::string getPassword() const { return password_; }
+private:
+    std::string sourceIp_;
+    std::string password_;
+};
+
+struct UserCheck {
+    UserCheck() = default;
+    virtual ~UserCheck() = default;
+    void add(UserCheck* next) { next_ = next; }
+    virtual bool check(User& user) {
+        if (next_) return next_->check(user);
+        return true;
+    }
+    UserCheck* next_ = nullptr;
+};
+
 void demo();
 } // namespace chainofresp
