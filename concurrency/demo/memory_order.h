f08a7e36cc007f037dd10a9dfed09a05c50a6c50
orbuluh <scuendless@gmail.com>
2022-08-16 21:09:48 +0800

    fix memory_order example indention

--- concurrency/demo/memory_order.h
+++ concurrency/demo/memory_order.h
@@ -1,42 +1,42 @@
 #pragma once
 #include <atomic>
 #include <iostream>
-#include <thread>
 #include <queue>
+#include <thread>
 
 namespace memory_order {
 
 namespace {
 std::string work = "notSet";
 std::atomic<bool> ready(false);
-}  // namespace
+} // namespace
 
 void consumer(std::memory_order flag) {
-  while (!ready.load(flag)) {
-  }
-  std::cout << work << std::endl;
+    while (!ready.load(flag)) {
+    }
+    std::cout << work << std::endl;
 }
 
 void producer(std::memory_order flag, std::string_view val) {
-  work = val;
-  ready = true;
+    work = val;
+    ready = true;
 }
 
 void demo() {
-  using mo = std::memory_order;
-  for (const auto& [flagP, flagC, val] : {
-           std::make_tuple(mo::seq_cst, mo::seq_cst, "seq_cst"),
-           std::make_tuple(mo::acquire, mo::release, "acquire_release"),
-           // std::make_tuple(mo::relaxed, mo::relaxed, "relaxed"),
-       }) {
-    std::thread prod(producer, flagP, val);
-    std::thread con(consumer, flagC);
-    prod.join();
-    con.join();
-  }
+    using mo = std::memory_order;
+    for (const auto& [flagP, flagC, val] : {
+             std::make_tuple(mo::seq_cst, mo::seq_cst, "seq_cst"),
+             std::make_tuple(mo::acquire, mo::release, "acquire_release"),
+             // std::make_tuple(mo::relaxed, mo::relaxed, "relaxed"),
+         }) {
+        std::thread prod(producer, flagP, val);
+        std::thread con(consumer, flagC);
+        prod.join();
+        con.join();
+    }
 }
 
-}  // namespace memory_order
+} // namespace memory_order
 
 namespace memory_order_sync_with_atomic {
 
@@ -44,7 +44,7 @@ namespace {
 std::queue<int> taskQ;
 std::atomic<bool> dataProduced;
 std::atomic<bool> dataConsumed;
-}
+} // namespace
 
 void dataProducer() {
     taskQ.push(10);
@@ -52,24 +52,28 @@ void dataProducer() {
 }
 
 void broker() {
-    while (!dataProduced.load(std::memory_order::acquire)) {;}
+    while (!dataProduced.load(std::memory_order::acquire)) {
+        ;
+    }
     dataConsumed.store(true, std::memory_order::release);
 }
 
 void dataConsumer() {
-    while (!dataConsumed.load(std::memory_order::acquire)) {;}
+    while (!dataConsumed.load(std::memory_order::acquire)) {
+        ;
+    }
     std::cout << taskQ.front() << '\n';
     taskQ.pop();
 }
 
 void demo() {
-  std::thread t1(dataConsumer);
-  std::thread t2(broker);
-  std::thread t3(dataProducer);
+    std::thread t1(dataConsumer);
+    std::thread t2(broker);
+    std::thread t3(dataProducer);
 
-  t1.join();
-  t2.join();
-  t3.join();
+    t1.join();
+    t2.join();
+    t3.join();
 }
 
-}  // namespace memory_order_sync_with_atomic
+} // namespace memory_order_sync_with_atomic
