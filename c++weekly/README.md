ca4c20d6ef7190442dc49506df7a516d613d665b
orbuluh <scuendless@gmail.com>
2022-09-28 21:10:19 +0800

    Ep 322 - Top 4 Places To Never Use const

--- c++weekly/README.md
+++ c++weekly/README.md
@@ -1,4 +1,35 @@
 # C++ weekly notes
 
-# Ep 322 - Top 4 Places To Never Use `const`
+# [Ep 322 - Top 4 Places To Never Use `const`](https://youtu.be/dGCxMmGvocE)
+- [code snippet](demo/no_const.h)
 - don't `const` non-reference return types (it breaks copy elision when return)
+- don't `const` local values that need take advantage of implicit move-on-return operations
+```cpp
+inline std::optional<S> make_value_3() {
+  const S s; // bad because this object and the return type
+             // are different! The optional will make a copy from the local s
+  return s;
+}
+```
+- Note: if you have multiple different objects that might be returned, then you are also relying on implicit move-on-return (aka automatic move)
+```cpp
+inline S make_value_3_1(bool option) {
+  if (option) {
+    const S s; // bad use of const
+    return s;
+  } else {
+    const S s2; // bad use of const
+    return s2; // better would be to return S{} directly!
+  }
+}
+```
+- don't `const` non-trivial value parameters that you might need to return directly from the function
+```cpp
+inline S make_value_2(const S s) // return statement makes this bad use of `const`
+{
+  return s; // because we return it, const is bad in function definition!
+}
+```
+- don't `const` *any* member data!
+  - it breaks implicit and explicit moves
+  - it breaks common use cases of assignment
--- c++weekly/demo/no_const.h
+++ c++weekly/demo/no_const.h
@@ -1,24 +1,82 @@
 #include <iostream>
 #include <memory>
+#include <optional>
 
 #include "util/pod.h"
 namespace no_const {
 
-inline S make_value() { return S{}; }
-// bad, non-& const return type!
-inline const S make_value_with_return_const() { return S{}; }
-
-void demo() {
-  {
-    puts("make value with return non-const");
-    S s = make_value();
-  }
-  {
-    puts("make value with return const");
-    S s2 = make_value_with_return_const();
+inline S return_ideal() { return S{}; }
+inline const S return_const() { return S{}; }
+inline const S return_from_const_local() {
+  const S s{};
+  return s;
+}
+inline S return_const_local_with_multiple_branch(bool option) {
+  if (option) {
+    const S s;  // bad use of const
+    return s;
+  } else {
+    const S s2;  // bad use of const
+    return s2;   // better would be to return S{} directly!
   }
 }
-
-
+inline std::optional<S> return_optional_from_const_local() {
+  const S s{};
+  return s;  // will use copy!
+}
+inline std::optional<S> return_optional_from_nonconst_local() {
+  S s{};
+  return s;  // will use move
+}
+inline S return_const_param(const S s) {
+  // const param will make 1 copy
+  // return will make another copy
+  return s;
+}
+void demo() {
+  {
+    puts("showcase pod");
+    S s;
+    auto s2 = s;
+    auto s3 = std::move(s);
+  }
+  {
+    puts("return non-const");
+    S s = return_ideal();
+  }
+  {
+    puts("return const non-reference");
+    // make no difference by clang 14.0.6, but could create extra
+    // construct and copy in other setup
+    S s = return_const();
+  }
+  {
+    puts("return from const local");
+    // make no difference by clang 14.0.6, but could create extra
+    // construct and copy in other setup
+    S s = return_from_const_local();
+  }
+  {
+    puts("return optional from branch 1");
+    // make no difference by clang 14.0.6, but could create extra
+    // construct and copy in other setup
+    S s = return_const_local_with_multiple_branch(0);
+    puts("return optional from branch 2");
+    S s2 = return_const_local_with_multiple_branch(1);
+  }
+  {
+    puts("return optional from const local");
+    auto s = return_optional_from_const_local();
+  }
+  {
+    puts("return optional from non-const local");
+    auto s = return_optional_from_nonconst_local();
+  }
+  {
+    puts("return const parameter");
+    S s;
+    S s2 = return_const_param(s);
+  }
+}
 
 }  // namespace no_const
