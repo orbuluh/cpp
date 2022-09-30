d2ec87458c6a4ae301c5beab116b1b5c75c202e7
orbuluh <scuendless@gmail.com>
2022-10-01 06:49:42 +0800

    unplayable ranges for clang 15.0.1

---
+++ c++weekly/demo/range_playground.h
@@ -0,0 +1,13 @@
+#include <ranges>
+#include <vector>
+
+// llvm Clang 15.0.1 not supported yet...
+
+namespace range_playground {
+
+void demo() {
+    //std::vector<int> vec{1, 2, 3, 4, 5, 6};
+    //auto result = std::ranges::all_of(vec, [](const int i) { return i % 2; });
+    //std::cout << result << '\n';
+}
+}  // namespace range_playground
--- c++weekly/main.cpp
+++ c++weekly/main.cpp
@@ -1,5 +1,7 @@
 #include "demo/no_const.h"
+//#include "demo/range_playground.h"  // compiler not really support
 
 int main(int argc, char* argv[]) {
-    no_const::demo();
+    //no_const::demo();
+    //range_playground::demo();
 }
