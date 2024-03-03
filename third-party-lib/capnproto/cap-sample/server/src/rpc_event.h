94f73fb5233fea6e9065d9aabe2af1df5ff90220
orbuluh <scuendless@gmail.com>
2024-03-03 18:12:49 +0900

    clean up and add readme notes

--- third-party-lib/capnproto/cap-sample/server/src/rpc_event.h
+++ third-party-lib/capnproto/cap-sample/server/src/rpc_event.h
@@ -27,33 +27,13 @@ class Server2ClientEvent {
  *  @brief This class defines the event interface from server to client.
  */
 class RPCEvent : public Client2ServerEvent,
-                 Server2ClientEvent,
+                 public Server2ClientEvent,
                  public kj::Refcounted {
  public:
-  /**
-   * @fn RPCEvent
-   * @brief Constructor
-   */
   RPCEvent();
-
-  /**
-   * @fn initialize
-   * @brief When the client calls `initialize`, then this function will be
-   * called.
-   */
   kj::Promise<void> initialize(InitializeContext context);
-
-  /**
-   * @fn subscribe
-   * @brief When client call `subscribe`, then this function will be called.
-   */
   kj::Promise<void> subscribe(SubscribeContext context);
 
-  /**
-   * @fn push_message_request
-   * @brief When the server wants to push back `push_message` to the client,
-   * call this function.
-   */
   virtual void push_message_request();
 
  private:
